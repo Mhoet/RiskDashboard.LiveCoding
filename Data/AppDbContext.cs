a460855d8b160717156e341269a2804332e6cc4c
Honoura Michael <homichael@unitybankng.com>
2026-08-28 12:05:42 +0100

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using RiskDashboard.LiveCoding.Models;
+
+namespace RiskDashboard.LiveCoding.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Risk> Risks { get; set; }
+    public DbSet<RiskControl> Controls { get; set; }
+    public DbSet<RiskAssessment> RiskAssessments { get; set; }
+}
